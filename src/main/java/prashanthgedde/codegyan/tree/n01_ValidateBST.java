366b79a40a5d591fb1e0cf36988d3d7ad2cb784b
Prashanth Gedde Narayanaswamy <prashanth.gedde@gmail.com>
2021-05-08 21:14:30 -0700

    Comments

--- src/main/java/prashanthgedde/codegyan/tree/n01_ValidateBST.java
+++ src/main/java/prashanthgedde/codegyan/tree/n01_ValidateBST.java
@@ -3,9 +3,17 @@ package prashanthgedde.codegyan.tree;
 public class n01_ValidateBST {
 
     public static boolean isValidBST(BinaryTreeNode root) {
+        // METHOD-1: recursive check with MIN, MAX thresholds
         return _isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+
+        // METHOD-2: TODO: recursive check with MIN, MAX for any object
+        // can we use null check and generic comparison of objects?
+
+        // METHOD-3: TODO: Can we use in-order traversal of tree and check if its in
+        // ascending order. If the order breaks any time we can fail the check.
     }
 
+    // METHOD-1: recursive check with MIN, MAX thresholds
     private static boolean _isValidBST(BinaryTreeNode node, long min, long max) {
         if(node == null) return true;
 
